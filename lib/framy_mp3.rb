5cbf1fae5e577942bdf4744c4df998e294ab92ba
Matthew Kobs <matt.kobs@cph.org>
2018-06-13 14:35:52 -0500

    [feature] Added Module method for merging mp3 files without reencoding (10m)

--- lib/framy_mp3.rb
+++ lib/framy_mp3.rb
@@ -4,4 +4,15 @@ require "framy_mp3/id3_tag"
 require "framy_mp3/file"
 
 module FramyMP3
+
+  def self.merge(*files)
+    raise ArgumentError, "all files must be FramyMP3::File" if files.any? { |file| !file.is_a?(FramyMP3::File) }
+    raise ArgumentError, "expected at least one file" unless files.count.positive?
+    outfile = files.first.dup
+    files[1..-1].each do |file|
+      outfile.frames.push(*file.frames)
+    end
+    outfile
+  end
+
 end
